94d33f4c477c03b56bc881bab0fd61f25e54c139
Andyngu98 <andyyn8@gmail.com>
2023-06-12 14:33:30 +0100

    prcing update

--- scripts/pricing.js
+++ scripts/pricing.js
@@ -7,6 +7,7 @@ toggleMenu(elements.burger, elements.nav);
 axios.get('https://nail-bar-56-default-rtdb.europe-west1.firebasedatabase.app/pricing/.json')
 .then((res) => {
   res.data['-MW02tFnNhIHr1Wh566-'].map((treatment) => {
+    console.log(treatment)
     const selector = document.querySelector(`.${treatment.key}`)
     for (let index = treatment.services.length - 1; index >= 0; index--) {
       const service = treatment.services[index];
